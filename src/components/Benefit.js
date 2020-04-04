c48af23a301d9c91914a535fda7e95f159bea074
irrellia <dicha.arkana03@gmail.com>
2020-04-04 07:38:30 +0700

    Added wrapper with 1080px max width

--- src/components/Benefit.js
+++ src/components/Benefit.js
@@ -11,34 +11,36 @@ import onboarding from "../assets/icon-onboarding.svg"
 function Benefit() {
   return (
     <section className="benefit">
-      <h1 className="benefit__title">Why choose easybank?</h1>
-      <p className="benefit__desc">
-        We leverage Open Banking to turn your bank account into your financial
-        hub.
-        <br />
-        Control your finances like never before.
-      </p>
-      <div className="benefit__cards">
-        <BenefitCard
-          img={online}
-          title="Online Banking"
-          desc="Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world."
-        />
-        <BenefitCard
-          img={budgeting}
-          title="Simple Budgeting"
-          desc="See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits."
-        />
-        <BenefitCard
-          img={onboarding}
-          title="Fast Onboarding"
-          desc="We don’t do branches. Open your account in minutes online and start taking control of your finances right away."
-        />
-        <BenefitCard
-          img={api}
-          title="Open API"
-          desc="Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier."
-        />
+      <div className="benefit__wrapper">
+        <h1 className="benefit__title">Why choose easybank?</h1>
+        <p className="benefit__desc">
+          We leverage Open Banking to turn your bank account into your financial
+          hub.
+          <br />
+          Control your finances like never before.
+        </p>
+        <div className="benefit__cards">
+          <BenefitCard
+            img={online}
+            title="Online Banking"
+            desc="Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world."
+          />
+          <BenefitCard
+            img={budgeting}
+            title="Simple Budgeting"
+            desc="See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits."
+          />
+          <BenefitCard
+            img={onboarding}
+            title="Fast Onboarding"
+            desc="We don’t do branches. Open your account in minutes online and start taking control of your finances right away."
+          />
+          <BenefitCard
+            img={api}
+            title="Open API"
+            desc="Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier."
+          />
+        </div>
       </div>
     </section>
   )
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,22 +9,24 @@ import Button from "./Button"
 function Hero() {
   return (
     <header className="hero">
-      <div className="hero__bg__wrapper">
-        <img src={background} alt="bg" className="hero__bg" />
-      </div>
-      <div className="hero__left">
-        <h1 className="hero__title">
-          Next generation <br /> digital banking
-        </h1>
-        <p className="hero__desc">
-          Take your financial life online. Your Easybank account <br /> will be
-          a one-stop-shop for spending, saving, <br /> budgeting, investing, and
-          much more.
-        </p>
-        <Button text="Request Invite" />
-      </div>
-      <div className="hero__right">
-        <img src={phones} alt="phones" className="hero__phones" />
+      <div className="hero__wrapper">
+        <div className="hero__bg__wrapper">
+          <img src={background} alt="bg" className="hero__bg" />
+        </div>
+        <div className="hero__left">
+          <h1 className="hero__title">
+            Next generation <br /> digital banking
+          </h1>
+          <p className="hero__desc">
+            Take your financial life online. Your Easybank account <br /> will
+            be a one-stop-shop for spending, saving, <br /> budgeting,
+            investing, and much more.
+          </p>
+          <Button text="Request Invite" />
+        </div>
+        <div className="hero__right">
+          <img src={phones} alt="phones" className="hero__phones" />
+        </div>
       </div>
     </header>
   )
